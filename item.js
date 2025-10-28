e78ebf94e1e76dd1cf9d83a86fb08fe4e83464f0
Tanmay3924 <tanmay.aj2004@gmail.com>
2025-10-28 18:49:10 +0530

    security

--- item.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const itemSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    description: String
-});
-
-module.exports = mongoose.model('Item', itemSchema);
